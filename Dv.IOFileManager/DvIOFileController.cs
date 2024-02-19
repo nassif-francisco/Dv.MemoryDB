f744e697557d2d1c8363308af6837fc0ae2b1d75
fgnassif <fsanchez@heuristyc.com>
2024-02-18 22:40:08 -0300

    work on DvFileController

---
+++ Dv.IOFileManager/DvIOFileController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.TypeConversion;
+using Dv.MemoryDB;
+
+namespace Dv.IOFileManager
+{
+    public class DvIOFileController<T> where T: DvTable
+    {
+        public DvIOFileController(string path)
+        {
+            Path = path;
+        }
+
+        public string Path { get; private set; }
+
+        public string? Class { get; private set; }
+        public string? FileContent { get; private set; }
+
+        public async Task<string> ReadCsvFile()
+        {
+            try
+            {
+                using (var sr = new StreamReader(Path))
+                {  
+                    FileContent = await sr.ReadToEndAsync();                                  
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                FileContent = ex.Message;
+            }
+            return FileContent;
+        }
+
+        public async IAsyncEnumerable<string> ReadCsvFileLineAsync()
+        {
+            //try
+            //{
+                using (var sr = new StreamReader(Path))
+                {
+                    FileContent = await sr.ReadLineAsync();
+                    yield return FileContent;
+                }
+            //}
+            //catch (FileNotFoundException ex)
+            //{
+            //    FileContent = ex.Message;
+            //}
+        }
+
+        public IEnumerable<string> ReadCsvFileLine()
+        {
+            //try
+            //{
+            using (var sr = new StreamReader(Path))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    FileContent = sr.ReadLine();
+                    yield return FileContent;
+                }
+            }
+               
+            //}
+            //catch (FileNotFoundException ex)
+            //{
+            //    FileContent = ex.Message;
+            //}
+        }
+
+        static async IAsyncEnumerable<string> ReadCharacters()
+        {
+            String result;
+            using (StreamReader reader = File.OpenText("existingfile.txt"))
+            {
+                Console.WriteLine("Opened file.");
+                result = await reader.ReadLineAsync();
+                yield return result;
+                Console.WriteLine("First line contains: " + result);
+            }
+        }
+    }
+}
---
+++ Dv.IOFileManager/DvIOFileManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Formats.Asn1;
+using System.Globalization;
+using System;
+using CsvHelper;
+using System.Xml.Linq;
+using System.Text;
+
+namespace Dv.IOFileManager
+{
+    public class DvIOFileManager
+    {
+        public DvIOFileManager(string path) 
+        {
+            Path = path;
+        }
+
+        public string Path { get; private set; }
+
+        public string Class { get; private set; }
+
+        public List<string> ReadCsvFile() {
+            var csvRows = System.IO.File.ReadAllLines(Path, Encoding.Default).ToList();
+
+            foreach (var row in csvRows.Skip(1))
+            {
+                var columns = row.Split(';');
+
+                var field1 = columns[0];
+                var field2 = columns[1];
+                var field3 = columns[2];
+            }
+
+            return csvRows;
+        }
+
+        //public IEnumerable<dynamic>? ReadCsvFile()
+        //{
+        //    IEnumerable<dynamic>? records = null;
+        //    var recx = new List<dynamic>();
+        //    using (var reader = new StreamReader(Path))
+        //    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        //    {
+        //        foreach(var rec in csv.GetRecords<dynamic>().ToList())
+        //        {
+        //            Console.WriteLine(rec);
+        //        }
+        //        //records = csv.GetRecords<dynamic>();
+        //        //recx= records.ToList();
+        //    }
+        //    foreach(var rec in records)
+        //    {
+        //        Console.WriteLine(rec);
+        //    }
+        //    var recordsList = records.ToList();
+        //    return recordsList;
+        //}
+
+        public List<string> WriteCsvFile(object instance)
+        {
+
+            return new List<string>();
+        }
+
+
+    }
+}
